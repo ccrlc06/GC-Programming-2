2c8313fef5e84ddd988c3877b99c7e3c78ca320e
Smiley98 <smileysofficialcinema@gmail.com>
2024-01-12 12:45:42 -0500

    Hello world

--- Assets/Scripts/Review.cs
+++ Assets/Scripts/Review.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Review : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("hello world!");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
     }
 }
