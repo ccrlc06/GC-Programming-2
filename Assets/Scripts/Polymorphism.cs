a85479106c314dcd5c57a74f3d5987c7af4533a0
Smiley98 <smileysofficialcinema@gmail.com>
2024-01-19 13:56:34 -0500

    Added manual vs automatic polymorphism

--- Assets/Scripts/Polymorphism.cs
+++ Assets/Scripts/Polymorphism.cs
@@ -42,18 +42,88 @@ public class Chicken : Animal
     }
 }
 
+public enum AnimalType
+{
+    DOG,
+    SHEEP,
+    COW,
+    CHICKEN
+}
+
 public class Polymorphism : MonoBehaviour
 {
+    void MakeSound(AnimalType animal)
+    {
+        switch (animal)
+        {
+            case AnimalType.DOG:
+                DogSound();
+                break;
+
+            case AnimalType.SHEEP:
+                SheepSound();
+                break;
+
+            case AnimalType.COW:
+                CowSound();
+                break;
+
+            case AnimalType.CHICKEN:
+                ChickenSound();
+                break;
+        }
+    }
+
+    void DogSound()
+    {
+        Debug.Log("WOOF!");
+    }
+
+    void SheepSound()
+    {
+        Debug.Log("Baaaaaaaaaaaaa");
+    }
+
+    void CowSound()
+    {
+        Debug.Log("MOOOOOOOOOOOOOOOOOOOOOOOOO");
+    }
+
+    void ChickenSound()
+    {
+        Debug.Log("Buck buck buck...");
+    }
+
     void Start()
     {
-        Animal dog = new Dog();
-        Animal sheep = new Sheep();
-        Animal cow = new Cow();
-        Animal chicken = new Chicken();
+        // New way (automatic)
+        Animal[] animals1 = {
+            new Dog(),
+            new Sheep(),
+            new Cow(),
+            new Chicken()
+        };
 
-        dog.MakeSound();
-        sheep.MakeSound();
-        cow.MakeSound();
-        chicken.MakeSound();
+        for (int i = 0; i < animals1.Length; i++)
+        {
+            animals1[i].MakeSound();
+        }
+
+        // Old way (manual)
+        AnimalType[] animals2 =
+        {
+            AnimalType.DOG, AnimalType.SHEEP, AnimalType.COW, AnimalType.CHICKEN
+        };
+
+        for (int i = 0; i < animals2.Length; i++)
+        {
+            MakeSound(animals2[i]);
+        }
+
+        // Homework hint: you'll want to change your weapon based on the pad you've collider with.
+        // For example,
+        // Weapon weapon = new Rifle();
+        // OnCollision:
+        //      if shotgun, weapon = new Shotgun
     }
 }
