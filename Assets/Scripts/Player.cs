0cad347248af87bbb23f1c31b4cea058bc3a349c
Smiley98 <smileysofficialcinema@gmail.com>
2024-01-19 12:48:29 -0500

    Added exam solution

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum WeaponType
+{
+    RIFLE,
+    SHOTGUN,
+    GRENADE
+}
+
 public class Player : MonoBehaviour
 {
+    public GameObject projectilePrefab;
+    WeaponType weaponType;
+
     //Rigidbody2D rb;
     float moveSpeed = 10.0f;    // Move at 10 units per second
     float turnSpeed = 360.0f;   // Turn at 360 degrees per second
@@ -31,14 +41,15 @@ public class Player : MonoBehaviour
         // A vector represents a direction.
         // We get our direction vector by taking whatever direction we want a rotation of 0 to be (Vector3.right in this case),
         // and multiply it by our transfor's rotation which is a quaternion.
-        Vector3 direction = transform.rotation * Vector3.right;
+        //Vector3 direction = transform.rotation * Vector3.right;
+        //Vector3 direction = transform.right;    // automatic version of the above
 
         // We can use trigonometry to convert directions to angles and angles to directions
         // (This is unnecessary for the actual implementation, just review)
         //float angle = Mathf.Atan2(direction.y, direction.x);
         //direction = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle));
         //Debug.Log(angle * Mathf.Rad2Deg);
-        Debug.DrawLine(transform.position, transform.position + direction * 10.0f);
+        Debug.DrawLine(transform.position, transform.position + transform.right * 10.0f);
 
         Vector3 velocity = Vector3.zero;
         if (Input.GetKey(KeyCode.W))
@@ -61,6 +72,70 @@ public class Player : MonoBehaviour
             velocity -= transform.up;
         }
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            switch (weaponType)
+            {
+                case WeaponType.RIFLE:
+                    CreateRifle(transform.right, moveSpeed * 5.0f);
+                    break;
+
+                case WeaponType.SHOTGUN:
+                    CreateShotgun(transform.right, moveSpeed * 2.5f);
+                    break;
+
+                case WeaponType.GRENADE:
+                    CreateGrenade(transform.right, moveSpeed);
+                    break;
+            }
+        }
+
         transform.position += velocity * moveSpeed * dt;
+        // "switch" is equivalent to the following (basically a fancy if-statement):
+        //if (weaponType == WeaponType.RIFLE)
+        //    color = Color.red;
+        //if (weaponType == WeaponType.SHOTGUN)
+        //    color = Color.green;
+        //if (weaponType == WeaponType.GRENADE)
+        //    color = Color.blue;
+        //GetComponent<SpriteRenderer>().color = color;
+    }
+
+    GameObject CreateProjectile(Vector3 direction, float speed, Color color)
+    {
+        GameObject projectile = Instantiate(projectilePrefab);
+        projectile.transform.position = transform.transform.position + direction;
+        projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
+        projectile.GetComponent<SpriteRenderer>().color = color; 
+        return projectile;
+    }
+
+    void CreateRifle(Vector3 direction, float speed)
+    {
+        CreateProjectile(direction, speed, Color.red);
+    }
+
+    void CreateShotgun(Vector3 direction, float speed)
+    {
+        CreateProjectile(Quaternion.Euler(0.0f, 0.0f, -30.0f) * direction, speed, Color.green);
+        CreateProjectile(direction, speed, Color.green);
+        CreateProjectile(Quaternion.Euler(0.0f, 0.0f, 30.0f) * direction, speed, Color.green);   
+    }
+
+    void CreateGrenade(Vector3 direction, float speed)
+    {
+        CreateProjectile(direction, speed, Color.blue);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Rifle"))
+            weaponType = WeaponType.RIFLE;
+
+        else if (collision.CompareTag("Shotgun"))
+            weaponType = WeaponType.SHOTGUN;
+
+        else if (collision.CompareTag("Grenade"))
+            weaponType = WeaponType.GRENADE;
     }
 }
